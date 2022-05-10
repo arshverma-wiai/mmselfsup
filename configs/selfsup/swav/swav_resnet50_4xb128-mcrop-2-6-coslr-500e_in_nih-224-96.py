faa971133ea2269bd6f8730d6e3b6cd021eaa93b
Arsh Verma <arsh@wadhwaniai.org>
2022-05-10 11:14:30 +0530

    Configs for swav

---
+++ configs/selfsup/swav/swav_resnet50_4xb128-mcrop-2-6-coslr-500e_in_nih-224-96.py
@@ -0,0 +1,27 @@
+import os
+
+_base_ = [
+    'swav_resnet50_4xb128-mcrop-2-6-coslr-200e_nih-224-96.py'
+]
+
+split_folder = "/scratchg/data/NIH-CXR/splits/SSL-50k/"
+data = dict(
+    samples_per_gpu=128,  
+    workers_per_gpu=4,
+    train=dict(
+        data_source=dict(
+            split_folder=split_folder,
+            ann_file=os.path.join(split_folder, "train.txt"),
+        ),
+    )) 
+
+runner = dict(type='EpochBasedRunner', max_epochs=500)
+
+model = dict(
+    backbone=dict(
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='/scratchg/data/NIH-CXR/checkpoints/selfsup/resnet50_imagenet_bs2k_epochs600.pth.tar',
+            prefix='backbone',
+        )),
+    )
