e892e841c9bab33f11c81df38c7a7e873d543dfa
Arsh Verma <arsh@wadhwaniai.org>
2022-05-14 19:19:45 +0530

    Configs for BYOL and MoCo v2

--- configs/selfsup/byol/byol_resnet50_4xb256-fp16-accum2-coslr-200e_nih_50k.py
+++ configs/selfsup/byol/byol_resnet50_4xb256-fp16-accum2-coslr-200e_nih_50k.py
@@ -18,7 +18,7 @@ custom_hooks = [
 # optimizer
 optimizer = dict(
     type='LARS',
-    lr=4.8,
+    lr=2.4,
     momentum=0.9,
     weight_decay=1e-6,
     paramwise_options={
---
+++ configs/selfsup/mocov2/mocov2_resnet50_4xb256-coslr-500e_nih_50k.py
@@ -0,0 +1,18 @@
+import os
+
+_base_ = [
+    'mocov2_resnet50_8xb32-coslr-200e_nih.py'
+]
+
+split_folder = "/scratchg/data/NIH-CXR/splits/SSL-50k/"
+data = dict(
+    samples_per_gpu=256,  
+    workers_per_gpu=10,
+    train=dict(
+        data_source=dict(
+            split_folder=split_folder,
+            ann_file=os.path.join(split_folder, "train.txt"),
+        ),
+    )) 
+
+runner = dict(type='EpochBasedRunner', max_epochs=500)
