942889d8eae5182d62721ffe37f4798646fbe159
Arsh Verma <arsh@wadhwaniai.org>
2022-06-27 20:29:19 +0530

    Configs for DenseNet SimCLR

---
+++ configs/selfsup/simclr/simclr_densenet161_4xb32-coslr-500e_in_sup_nih_v2a.py
@@ -0,0 +1,27 @@
+import os
+
+_base_ = 'simclr_densenet161_4xb32-coslr-200e_nih.py'
+
+# dataset summary
+split_folder = "/scratchg/data/NIH-CXR/splits/v2-a/"
+data = dict(
+    samples_per_gpu=32,  
+    workers_per_gpu=4,
+    train=dict(
+        data_source=dict(
+            split_folder=split_folder,
+            ann_file=os.path.join(split_folder, "train.txt"),
+        ),
+    )) 
+
+runner = dict(max_epochs=500)
+
+optimizer = dict(lr=0.15)
+
+model = dict(
+    backbone=dict(
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='/scratchg/data/NIH-CXR/checkpoints/selfsup/densenet161-supervised-imagenet-updated.pth',
+        )),
+    )
